771ad57e8f0f7950e1f0d32a50cd1fb804e048de
JG <jijinggang@gmail.com>
2012-10-11 11:50:47 +0800

    Update simpleconfig.go

--- simpleconfig.go
+++ simpleconfig.go
@@ -1,176 +1,176 @@
 package simpleconfig
 
 import (
-    "bufio"
-    "bytes"
-    "io"
-    "os"
-    "reflect"
-    "strconv"
-    "strings"
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 type Config struct {
-    data map[string]string
-    err  os.Error
+	data map[string]string
+	err  error
 }
 
 func read(r io.Reader) *Config {
-    data := make(map[string]string)
-    reader := bufio.NewReader(r)
-    for {
-        line, err := reader.ReadString('\n')
-        if err != nil {
-            break
-        }
+	data := make(map[string]string)
+	reader := bufio.NewReader(r)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
 
-        line = strings.TrimSpace(line)
-        //remove blank lines
-        if len(line) == 0 {
-            continue
-        }
-        //remove comment lines
-        if strings.HasPrefix(line, "#") {
-            continue
-        }
-    	//config file is same with .properties file
-        pos := strings.Index(line, "=")
-        if pos > 0 {
+		line = strings.TrimSpace(line)
+		//remove blank lines
+		if len(line) == 0 {
+			continue
+		}
+		//remove comment lines
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+		//config file is same with .properties file
+		pos := strings.Index(line, "=")
+		if pos > 0 {
 			k := strings.Trim(line[:pos], " ")
 			v := strings.Trim(line[pos+1:], " ")
-            data[k] = v
+			data[k] = v
 		}
-        //parts := strings.Split(line, " ", 2)
-        //if len(parts) == 2 {
-        //   data[parts[0]] = parts[1]
-        //}
-    }
+		//		parts := strings.SplitN(line, "=", 2)
+		//		if len(parts) == 2 {
+		//			data[parts[0]] = parts[1]
+		//		}
+	}
 
-    var ret Config
-    ret.data = data
-    return &ret
+	var ret Config
+	ret.data = data
+	return &ret
 }
 
 func Read(reader io.Reader) *Config {
-    return read(reader)
+	return read(reader)
 }
 
 func ReadFile(filename string) *Config {
-    var ret Config
-    f, err := os.Open(filename, os.O_RDONLY, 0666)
-    if err != nil {
-        ret.err = err
-        return &ret
+	var ret Config
+	f, err := os.Open(filename)
+	if err != nil {
+		ret.err = err
+		return &ret
 
-    }
-    //read config from the file
-    defer f.Close()
-    return read(f)
+	}
+	//read config from the file
+	defer f.Close()
+	return read(f)
 }
 
 func ReadString(s string) *Config {
-    //read config from the string
-    buffer := bytes.NewBufferString(s)
-    return read(buffer)
+	//read config from the string
+	buffer := bytes.NewBufferString(s)
+	return read(buffer)
 }
 
-func writeTo(s string, val reflect.Value) os.Error {
-    switch v := val.(type) {
-    // if we're writing to an interace value, just set the byte data
-    // TODO: should we support writing to a pointer?
-    case *reflect.InterfaceValue:
-        v.Set(reflect.NewValue(s))
-    case *reflect.BoolValue:
-        if strings.ToLower(s) == "false" || s == "0" {
-            v.Set(false)
-        } else {
-            v.Set(true)
-        }
-    case *reflect.IntValue:
-        i, err := strconv.Atoi64(s)
-        if err != nil {
-            return err
-        }
-        v.Set(i)
-    case *reflect.UintValue:
-        ui, err := strconv.Atoui64(s)
-        if err != nil {
-            return err
-        }
-        v.Set(ui)
-    case *reflect.FloatValue:
-        f, err := strconv.Atof64(s)
-        if err != nil {
-            return err
-        }
-        v.Set(f)
+func writeTo(s string, val reflect.Value) error {
+	switch v := val; v.Kind() {
+	// if we're writing to an interace value, just set the byte data
+	// TODO: should we support writing to a pointer?
+	case reflect.Interface:
+		v.Set(reflect.ValueOf(s))
+	case reflect.Bool:
+		if strings.ToLower(s) == "false" || s == "0" {
+			v.SetBool(false)
+		} else {
+			v.SetBool(true)
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		i, err := strconv.ParseInt(s, 10, 64)
+		if err != nil {
+			return err
+		}
+		v.SetInt(i)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		ui, err := strconv.ParseUint(s, 10, 64)
+		if err != nil {
+			return err
+		}
+		v.SetUint(ui)
+	case reflect.Float32, reflect.Float64:
+		f, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			return err
+		}
+		v.SetFloat(f)
 
-    case *reflect.StringValue:
-        v.Set(s)
-    case *reflect.SliceValue:
-        typ := v.Type().(*reflect.SliceType)
-        if _, ok := typ.Elem().(*reflect.UintType); ok {
-            v.Set(reflect.NewValue([]byte(s)).(*reflect.SliceValue))
-        }
-    }
-    return nil
+	case reflect.String:
+		v.SetString(s)
+	case reflect.Slice:
+		typ := v.Type()
+		if typ.Elem().Kind() == reflect.Uint || typ.Elem().Kind() == reflect.Uint8 || typ.Elem().Kind() == reflect.Uint16 || typ.Elem().Kind() == reflect.Uint32 || typ.Elem().Kind() == reflect.Uint64 || typ.Elem().Kind() == reflect.Uintptr {
+			v.Set(reflect.ValueOf([]byte(s)))
+		}
+	}
+	return nil
 }
 
 // matchName returns true if key should be written to a field named name.
 func matchName(key, name string) bool {
-    return strings.ToLower(key) == strings.ToLower(name)
+	return strings.ToLower(key) == strings.ToLower(name)
 }
 
-func writeToContainer(dst reflect.Value, data map[string]string) os.Error {
-    switch v := dst.(type) {
-    case *reflect.PtrValue:
-        return writeToContainer(reflect.Indirect(v), data)
-    case *reflect.InterfaceValue:
-        return writeToContainer(v.Elem(), data)
-    case *reflect.MapValue:
-        if _, ok := v.Type().(*reflect.MapType).Key().(*reflect.StringType); !ok {
-            return os.NewError("Invalid map key type")
-        }
-        elemtype := v.Type().(*reflect.MapType).Elem()
-        for pk, pv := range data {
-            mk := reflect.NewValue(pk)
-            mv := reflect.MakeZero(elemtype)
-            writeTo(pv, mv)
-            v.SetElem(mk, mv)
-        }
-    case *reflect.StructValue:
-        for pk, pv := range data {
-            //try case sensitive match
-            field := v.FieldByName(pk)
-            if field != nil {
-                writeTo(pv, field)
-            }
+func writeToContainer(dst reflect.Value, data map[string]string) error {
+	switch v := dst; v.Kind() {
+	case reflect.Ptr:
+		return writeToContainer(reflect.Indirect(v), data)
+	case reflect.Interface:
+		return writeToContainer(v.Elem(), data)
+	case reflect.Map:
+		if v.Type().Key().Kind() != reflect.String {
+			return errors.New("Invalid map key type")
+		}
+		elemtype := v.Type().Elem()
+		for pk, pv := range data {
+			mk := reflect.ValueOf(pk)
+			mv := reflect.Zero(elemtype)
+			writeTo(pv, mv)
+			v.SetMapIndex(mk, mv)
+		}
+	case reflect.Struct:
+		for pk, pv := range data {
+			//try case sensitive match
+			field := v.FieldByName(pk)
+			if field.IsValid() {
+				writeTo(pv, field)
+			}
 
-            //try case insensitive matching
-            field = v.FieldByNameFunc(func(s string) bool { return matchName(pk, s) })
-            if field != nil {
-                writeTo(pv, field)
-            }
+			//try case insensitive matching
+			field = v.FieldByNameFunc(func(s string) bool { return matchName(pk, s) })
+			if field.IsValid() {
+				writeTo(pv, field)
+			}
 
-        }
-    default:
-        return os.NewError("Invalid container type")
-    }
-    return nil
+		}
+	default:
+		return errors.New("Invalid container type")
+	}
+	return nil
 }
 
-func (c *Config) Get() (map[string]string, os.Error) {
-    if c.err != nil {
-        return nil, c.err
-    }
-    return c.data, nil
+func (c *Config) Get() (map[string]string, error) {
+	if c.err != nil {
+		return nil, c.err
+	}
+	return c.data, nil
 }
 
+func (c *Config) Unmarshal(dst interface{}) error {
+	if c.err != nil {
+		return c.err
+	}
 
-func (c *Config) Unmarshal(dst interface{}) os.Error {
-    if c.err != nil {
-        return c.err
-    }
-
-    return writeToContainer(reflect.NewValue(dst), c.data)
+	return writeToContainer(reflect.ValueOf(dst), c.data)
 }
